5431da866fdcb27784de712523cc945949c62e68
Vikteris <63242034+Vikteris@users.noreply.github.com>
2020-11-11 13:00:16 +0200

    responsive issues

--- src/components/Contacts/ContactsElements.js
+++ src/components/Contacts/ContactsElements.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Form = styled.form`
     width: 100%;
-    height: 80vh;
+    /* height: 80vh; */
     display: flex;
     flex-direction: column;
     justify-content: center; 
